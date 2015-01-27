07e634be5e522486feeb4250301bdc5352c61236
RonShipman <shipmanr@msoe.edu>
2015-01-27 16:03:03 -0600

    State Machine Added
    Added state machine

---
+++ Design/Enums/RobotState.m
@@ -0,0 +1,9 @@
+classdef(Enumeration) RobotState < Simulink.IntEnumType
+    %ROBOTSTATE Summary of this class goes here
+    %   Detailed explanation goes here
+    enumeration
+        Wait(0)
+        Stop(1)
+    end
+end
+
