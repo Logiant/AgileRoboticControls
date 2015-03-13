434c9b21aa783f1b3102ba5023acb7a783fd00f3
Logiant <beaverl@msoe.edi>
2015-03-12 22:23:31 -0500

    Created initial eclipse project for HMI
    Initial project with some methods and comments/documentation
    Uses Java 7 in Eclipse
    Requires a way to read controller input and communicate with Xbee (DLLs
    possible)

---
+++ System Modelling/Control/DogeBot HMI/src/arc/msoe/hmi/comms/WriteThread.java
@@ -0,0 +1,11 @@
+package arc.msoe.hmi.comms;
+/**
+ * A thread that writes data from RTComms to the Xbee controller
+ * 
+ * 3/12/2015
+ * @author beaverl
+ *
+ */
+public class WriteThread extends Thread {
+
+}
